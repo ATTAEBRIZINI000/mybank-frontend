71166f735f5b90de3a57b8f7ec326d64cd4b29c2
ATTAEBRIZINI000 <xovangpa@gmail.com>
2025-08-09 11:09:18 +0100

    fixe an erorr

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4000,
+    port: 3000,
     host: true,
     watch: {
       usePolling: true,
